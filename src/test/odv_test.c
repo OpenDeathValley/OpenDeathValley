48a67e9ed35e8ebf74688aa6fdcffb0d31a45247
w4kfu <gw4kfu@gmail.com>
2014-11-13 09:25:54 +0100

    add new test

--- src/test/odv_test.c
+++ src/test/odv_test.c
@@ -1,11 +1,10 @@
 #include "odv_test.h"
-#include "odv_resource_handler.h"
 
-void odv_test_resource(void)
+void odv_test_resource(const char *filename)
 {
     struct ODVResourceFile *rfile = NULL;
 
-    rfile = odv_resource_open("Expressions.res");
+    rfile = odv_resource_open(filename);
     if (rfile == NULL) {
         return;
     }
@@ -13,9 +12,38 @@ void odv_test_resource(void)
     odv_resource_close(rfile);
 }
 
-int main(void)
+void odv_test_script(const char *filename)
 {
-    printf("[+] Launching test\n");
-    odv_test_resource();
+    (void)filename;
+
+}
+
+void help(void)
+{
+    printf("OPTION:\n");
+    printf("\t-r: resource file (*.res)\n");
+    printf("\t-s: script file (*.scb)\n");
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3) {
+        fprintf(stderr, "[-] Usage: %s OPTION FILE\n", argv[1]);
+        help();
+    }
+    if (argv[1][0] != '-') {
+        help();
+    }
+    switch (argv[1][1]) {
+        case 'r':
+            odv_test_resource(argv[2]);
+            break;
+        case 's':
+            odv_test_script(argv[2]);
+            break;
+        default:
+            help();
+    }
     return 0;
 }
--- src/test/odv_test.h
+++ src/test/odv_test.h
@@ -1,5 +1,7 @@
 #ifndef OPENDV_TEST_H
 #define OPENDV_TEST_H
 
+#include "odv_resource_handler.h"
+
 
 #endif /* OPENDV_TEST_H */
